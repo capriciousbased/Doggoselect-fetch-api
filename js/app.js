3a5eb0176272bb566cf9a10a0fc324bfbca044fe
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-01-29 00:47:36 +0100

    add breadList

--- js/app.js
+++ js/app.js
@@ -5,16 +5,31 @@ const form = document.querySelector('form');
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+fetch('https://dog.ceo/api/breeds/list')
+.then(response => response.json())
+.then(data => generateOption(data.message))
 
 fetch('https://dog.ceo/api/breeds/image/random')
   .then(res => res.json())
-  .then(data => console.log(data.message))
+  .then(data => generateImage(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+  function generateImage(data){
+    const html = `
+    <img src='${data}' alt>
+    <p> Click to view image of ${select.value}s</p>
+    `;
+    card.innerHTML = html; 
+  }
 
-
+  function generateOption(data){
+    const options = data.map(item => `
+      <option value = '${item}'> ${item} </option>
+    `).join('');
+    select.innerHTML = options;  
+  }
 
 // ------------------------------------------
 //  EVENT LISTENERS
