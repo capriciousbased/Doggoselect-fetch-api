5d3fbab1ae974072033fbd5f6afe58d6a262020e
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-01-29 22:30:41 +0100

    add Reusable fetch function

--- js/app.js
+++ js/app.js
@@ -1,43 +1,61 @@
-const select = document.getElementById('breeds');
-const card = document.querySelector('.card'); 
-const form = document.querySelector('form');
+const select = document.getElementById("breeds");
+const card = document.querySelector(".card");
+const form = document.querySelector("form");
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-fetch('https://dog.ceo/api/breeds/list')
-.then(response => response.json())
-.then(data => generateOption(data.message))
+function fetchData(url) {
+  return fetch(url).then((res) => res.json());
+}
 
-fetch('https://dog.ceo/api/breeds/image/random')
-  .then(res => res.json())
-  .then(data => generateImage(data.message))
+fetchData("https://dog.ceo/api/breeds/list").then((data) =>
+  generateOption(data.message)
+);
+
+fetchData("https://dog.ceo/api/breeds/image/random").then((data) =>
+  generateImage(data.message)
+);
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-  function generateImage(data){
-    const html = `
+function generateImage(data) {
+  const html = `
     <img src='${data}' alt>
     <p> Click to view image of ${select.value}s</p>
     `;
-    card.innerHTML = html; 
-  }
+  card.innerHTML = html;
+}
 
-  function generateOption(data){
-    const options = data.map(item => `
+function generateOption(data) {
+  const options = data
+    .map(
+      (item) => `
       <option value = '${item}'> ${item} </option>
-    `).join('');
-    select.innerHTML = options;  
-  }
+    `
+    )
+    .join("");
+  select.innerHTML = options;
+}
 
+function fetchBreedImage() {
+  const breed = select.value;
+  const img = card.querySelector("img");
+  const p = card.querySelector("p");
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`).then((data) => {
+    img.src = data.message;
+    img.alt = breed;
+    p.textContent = ` click to view more ${breed}s`;
+  });
+}
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
-
-
+select.addEventListener("change", fetchBreedImage);
+card.addEventListener("click", fetchBreedImage);
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
-
